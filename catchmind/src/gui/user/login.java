de4d3fac9cf9f47958ece0d8b85a989af801149d
JeonJeongWook <jjwook324gmail.com>
2019-05-28 00:25:04 +0900

    GUI

--- catchmind/src/gui/user/login.java
+++ catchmind/src/gui/user/login.java
@@ -1,6 +1,8 @@
 package gui.user;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,41 +17,88 @@ public class login {
 	}
 }
 
-class login1 extends JFrame{
+class login1 extends JFrame implements ActionListener{
 	
 	
-	JPanel ptitle, pid, pps;
+	JPanel ptitle, pid, pps, pbtn;
 	JLabel lbtitle, lbid, lbpw;
 	JButton btnlogin, btnregister;
 	JTextField tfid, tfpw;
+	String id, pw;
 	
 	login1() {
+		//객체생성
 		ptitle = new JPanel(new FlowLayout());
 		pid = new JPanel(new FlowLayout());
 		pps = new JPanel(new FlowLayout());
+		pbtn = new JPanel(new FlowLayout());
+		
 		lbtitle = new JLabel("로그인을 해주세요");
 		lbid = new JLabel("ID");
 		lbpw = new JLabel("PW");
-		tfid = new JTextField(20);
-		tfpw = new JTextField(20);
+		
+		tfid = new JTextField(15);
+		tfpw = new JTextField(15);
+		
 		btnlogin = new JButton("로그인");
 		btnregister = new JButton("회원가입");
 		
+		//버튼
+		btnlogin.addActionListener(this);		//로그인
+		btnregister.addActionListener(this);	//회원가입
+		
+		//Panel에 추가
 		ptitle.add(lbtitle);
+		
 		pid.add(lbid);
 		pid.add(tfid);
 		
 		pps.add(lbpw);
 		pps.add(tfpw);
+		
+		pbtn.add(btnlogin);
+		pbtn.add(btnregister);
+		
+//		tfid.setText("");
+//		tfpw.setText("");
 	}
+	
 	void display() {
 		setVisible(true);
 		setLayout(new FlowLayout());
 		setTitle("놓지마! 정신줄!!");
-		setSize(500,400);
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(300,400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		add(ptitle);
 		add(pid);
 		add(pps);
+		add(pbtn);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		//로그인 버튼을 눌렀을 시
+		if(e.getSource() == btnlogin) {
+			System.out.println("[login]로그인 버튼이에요");
+			id = tfid.getText();
+			pw = tfpw.getText();
+			
+			//아이디, 비밀번호 값이 없으면
+			if(id.equals("") || pw.equals("")) {
+				notice notice = new notice();
+				notice.text("아이디 및 비밀번호를 써주세요");
+				notice.display("로그인 실패");
+			}
+		}
+
+		//회원가입 버튼을 눌렀을 시
+		if(e.getSource() == btnregister) {
+			System.out.println("[login]회원가입 하러 갈게요");
+			
+			//회원가입 화면으로 이동
+			register1 reg = new register1();
+			reg.display();
+		}
 	}
 }
---
+++ catchmind/src/gui/user/notice.java
@@ -0,0 +1,41 @@
+package gui.user;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class notice extends JFrame implements ActionListener{
+	JLabel lbnotice;
+	JButton btnclose;
+
+	//실패시 라벨
+	void text(String text) {
+		lbnotice = new JLabel(text);
+		add(lbnotice);
+	}
+	
+	//로그인 실패 화면
+	void display(String title) {
+		btnclose = new JButton("닫기");
+		btnclose.addActionListener(this);
+		add(btnclose);
+		
+		setTitle(title);
+		setVisible(true);
+		setLayout(new FlowLayout());
+		setSize(300,200);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == btnclose) {
+			dispose();
+		}
+	}
+}
