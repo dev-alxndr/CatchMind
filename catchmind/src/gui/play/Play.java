beaba1b103cde9950bc0d1c6b13ce93838265ec5
JeonJeongWook <jjwook324gmail.com>
2019-05-29 01:09:58 +0900

    GUI

--- catchmind/src/gui/play/Play.java
+++ catchmind/src/gui/play/Play.java
@@ -1,7 +1,10 @@
 package gui.play;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,6 +16,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import javafx.scene.layout.Border;
+
 public class Play {
 
 	public static void main(String[] args) {
@@ -23,16 +28,13 @@ public class Play {
 }
 class MakeRoom extends JFrame implements ActionListener{
 	//전체 패널
-	JPanel p_layout = new JPanel(new BorderLayout(5,5));
-	
+	JPanel p_layout;
 	//유저 패널
-	JPanel p_user1, p_user2, p_user3, p_user4;
 	JPanel p_leftuser, p_rightuser;
-	
-	//뒤로가기 버튼, 단어, 그림판
-	JPanel p_menubar, p_paintCanvas, p_word;
-	
-	
+	JPanel p_user1, p_user2, p_user3, p_user4;
+	//뒤로가기 버튼,중앙패널(단어, 그림판),하단(채팅)
+	JPanel p_menubar, p_center, p_paintCanvas, p_answerWord, p_chat;
+
 	//유저 이름
 	JLabel lb_userName1, lb_userName2, lb_userName3, lb_userName4;
 	
@@ -44,36 +46,77 @@ class MakeRoom extends JFrame implements ActionListener{
 	JTextArea ta_chatting;
 	JTextField tf_msg;
 
+	
+	//삭제할 버튼(자리 확인용)
+	JButton btn1 = new JButton("확인");
 	MakeRoom(){
-		p_menubar = new JPanel(new FlowLayout());
+		p_layout = new JPanel(new BorderLayout());
+		//위쪽
+		p_menubar = new JPanel();
+		//중앙
+		p_center = new JPanel();
+			p_answerWord = new JPanel();
+			p_paintCanvas = new JPanel();
+		p_answerWord = new JPanel();
+		p_paintCanvas = new JPanel();
+		//왼쪽
+		p_leftuser = new JPanel();
+			p_user1 = new JPanel();
+			p_user2 = new JPanel();
+		//오른쪽
+		p_rightuser = new JPanel();
+			p_user3 = new JPanel();
+			p_user4 = new JPanel();
+		//하단
+		p_chat = new JPanel();
 		
-		p_user1 = new JPanel(new FlowLayout());
-		p_user2 = new JPanel(new FlowLayout());
-		p_user3 = new JPanel(new FlowLayout());
-		p_user4 = new JPanel(new FlowLayout());
+		lb_userName1 = new JLabel("유저1");
+		lb_userName2 = new JLabel("유저2");
+		lb_userName3 = new JLabel("유저3");
+		lb_userName4 = new JLabel("유저4");
 		
-		
-		lb_userName1 = new JLabel("lb_userName1");
-		lb_userName2 = new JLabel("lb_userName2");
-		lb_userName3 = new JLabel("lb_userName3");
-		lb_userName4 = new JLabel("lb_userName4");
-		
-		ta_chatting = new JTextArea(20,20);
+		ta_chatting = new JTextArea(15,15);
 		tf_msg = new JTextField(15);
-		
+		btn_logout = new JButton("로그아웃");
 		btn_send = new JButton("보내기");
 		
-//		p_layout.add();
+		p_layout.add(p_menubar,BorderLayout.NORTH);
+		p_menubar.add(btn_logout);
+		add(p_layout);
+		
+		p_layout.add(p_leftuser,BorderLayout.WEST);
+		p_leftuser.add(p_user1);
+		p_leftuser.add(p_user2);
 		p_user1.add(lb_userName1);
 		p_user2.add(lb_userName2);
+		
+		p_layout.add(p_rightuser,BorderLayout.EAST);
+		p_rightuser.add(p_user3);
+		p_rightuser.add(p_user4);
 		p_user3.add(lb_userName3);
 		p_user4.add(lb_userName4);
 		
-		p_user1.add(ta_chatting);
-		p_user1.add(tf_msg);
-		p_user1.add(btn_send);
+		p_layout.add(p_center,BorderLayout.CENTER);
+		p_center.add(p_paintCanvas);
+		p_paintCanvas.add(btn1);
 		
+		p_layout.add(p_chat,BorderLayout.SOUTH);
+		p_chat.add(ta_chatting);
+		p_chat.add(tf_msg);
+		p_chat.add(btn_send);
+		
+//		add(lb_userName1);
+//		add(lb_userName2);
+//		add(lb_userName3);
+//		add(lb_userName4);
+//		add(btn_logout);
+//		add(btn_send);
+//		add(ta_chatting);
+//		add(tf_msg);
+		
+		//버튼 설정
 		btn_send.addActionListener(this);
+		btn_logout.addActionListener(this);
 	}
 	
 	void display() {
@@ -85,16 +128,17 @@ class MakeRoom extends JFrame implements ActionListener{
 		setResizable(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		add(p_user1);
-		add(p_user2);
-		add(p_user3);
-		add(p_user4);
+//		add(p_user1);
+//		add(p_user2);
+//		add(p_user3);
+//		add(p_user4);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		String chat = "";
+		//보내기 버튼
 		if(e.getSource() == btn_send)
 		{
+			String chat = "";
 			chat = tf_msg.getText();
 			
 			//널값이 아니면
@@ -107,5 +151,14 @@ class MakeRoom extends JFrame implements ActionListener{
 				
 			}
 		}
+		
+		//로그아웃 버튼
+		if(e.getSource() == btn_logout)
+		{
+			dispose();
+			setVisible(false);
+		}
+		
+		
 	}
 }
