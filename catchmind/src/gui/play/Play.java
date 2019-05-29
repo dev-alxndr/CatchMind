26c57693d387a13bdefaad2ea0ce89065f552baf
JeonJeongWook <jjwook324gmail.com>
2019-05-29 09:14:33 +0900

    GUI

--- catchmind/src/gui/play/Play.java
+++ catchmind/src/gui/play/Play.java
@@ -2,12 +2,14 @@ package gui.play;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -33,11 +35,12 @@ class MakeRoom extends JFrame implements ActionListener{
 	JPanel p_leftuser, p_rightuser;
 	JPanel p_user1, p_user2, p_user3, p_user4;
 	//뒤로가기 버튼,중앙패널(단어, 그림판),하단(채팅)
-	JPanel p_menubar, p_center, p_paintCanvas, p_answerWord, p_chat;
+	JPanel p_menubar, p_center, p_paintCanvas, p_answerWord, p_chat, p_chatlog, p_send;
 
 	//유저 이름
 	JLabel lb_userName1, lb_userName2, lb_userName3, lb_userName4;
-	
+	JLabel lb_answerWord;
+
 //	점수 스코어 채팅시 -10점 정답시 +100
 //	JLabel lb_userScore1, lb_userScore2, lb_userScore3, lb_userScore4;
 //	JLabel lb_scoreView1, lb_scoreView2, lb_scoreView3, lb_scoreView4;
@@ -48,40 +51,46 @@ class MakeRoom extends JFrame implements ActionListener{
 
 	
 	//삭제할 버튼(자리 확인용)
-	JButton btn1 = new JButton("확인");
+	JButton btn1 = new JButton("자리 확인용 버튼");
 	MakeRoom(){
 		p_layout = new JPanel(new BorderLayout());
 		//위쪽
 		p_menubar = new JPanel();
 		//중앙
-		p_center = new JPanel();
+		p_center = new JPanel(new GridLayout(3, 1));
 			p_answerWord = new JPanel();
 			p_paintCanvas = new JPanel();
-		p_answerWord = new JPanel();
+		p_answerWord = new JPanel(new FlowLayout());
 		p_paintCanvas = new JPanel();
 		//왼쪽
-		p_leftuser = new JPanel();
+		p_leftuser = new JPanel(new GridLayout(2, 1));
 			p_user1 = new JPanel();
 			p_user2 = new JPanel();
 		//오른쪽
-		p_rightuser = new JPanel();
+		p_rightuser = new JPanel(new GridLayout(2, 1));
 			p_user3 = new JPanel();
 			p_user4 = new JPanel();
 		//하단
-		p_chat = new JPanel();
+		p_chat = new JPanel(new GridLayout(2, 1));
+			p_chatlog = new JPanel();
+			p_send = new JPanel(new GridLayout(1, 2));
 		
 		lb_userName1 = new JLabel("유저1");
 		lb_userName2 = new JLabel("유저2");
 		lb_userName3 = new JLabel("유저3");
 		lb_userName4 = new JLabel("유저4");
 		
+		
 		ta_chatting = new JTextArea(15,15);
 		tf_msg = new JTextField(15);
 		btn_logout = new JButton("로그아웃");
 		btn_send = new JButton("보내기");
 		
+		lb_answerWord = new JLabel("답이 표시되는 곳");
+		
 		p_layout.add(p_menubar,BorderLayout.NORTH);
 		p_menubar.add(btn_logout);
+		
 		add(p_layout);
 		
 		p_layout.add(p_leftuser,BorderLayout.WEST);
@@ -97,13 +106,25 @@ class MakeRoom extends JFrame implements ActionListener{
 		p_user4.add(lb_userName4);
 		
 		p_layout.add(p_center,BorderLayout.CENTER);
+		p_center.add(p_answerWord);
+			p_answerWord.add(lb_answerWord);
+			
 		p_center.add(p_paintCanvas);
-		p_paintCanvas.add(btn1);
+		p_center.add(p_chat);
+			p_chat.add(p_chatlog);
+				p_chatlog.add(ta_chatting);
+			p_chat.add(p_send);
+				p_send.add(tf_msg);
+				p_send.add(btn_send);
 		
-		p_layout.add(p_chat,BorderLayout.SOUTH);
-		p_chat.add(ta_chatting);
-		p_chat.add(tf_msg);
-		p_chat.add(btn_send);
+		p_layout.add(btn1, BorderLayout.SOUTH);
+		
+//		p_layout.add(p_chat,BorderLayout.SOUTH);
+//		p_chat.add(p_chatlog);
+//		p_chatlog.add(ta_chatting);
+//		p_chat.add(p_send);
+//			p_send.add(tf_msg);
+//			p_send.add(btn_send);
 		
 //		add(lb_userName1);
 //		add(lb_userName2);
@@ -113,6 +134,24 @@ class MakeRoom extends JFrame implements ActionListener{
 //		add(btn_send);
 //		add(ta_chatting);
 //		add(tf_msg);
+		
+		p_layout.setPreferredSize(new Dimension(600, 700));
+		p_layout.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_menubar.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_leftuser.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_rightuser.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_user1.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_user2.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_user3.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_user4.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_center.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_chat.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_chatlog.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_paintCanvas.setBorder(BorderFactory.createLineBorder(Color.black));
+		p_answerWord.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		
+		
 		
 		//버튼 설정
 		btn_send.addActionListener(this);
