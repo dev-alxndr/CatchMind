dcd7c2c0b07644ccd122d9a4de8c969e1f47d4d2
JeonJeongWook <jjwook324gmail.com>
2019-05-30 10:13:23 +0900

    Making Canvas1

--- catchmind/src/gui/play/Draw.java
+++ catchmind/src/gui/play/Draw.java
@@ -2,6 +2,7 @@ package gui.play;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -14,36 +15,44 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Draw extends JFrame implements ActionListener, MouseMotionListener{
+	JPanel p_layout, p_drawCanvas, p_btnbar;
+	JButton btn_clear;
+	MakeCanvas makeCanvas;
 
 	public static void main(String[] args) {
 		Draw draw = new Draw();
+		draw.display();
 	}
 	
-	JPanel p_drawCanvas, p_btnbar;
-	JButton btn_clear;
-	NewCanvas newCanvas;
 	
-	public Draw() {
+	Draw() {
+		p_layout = new JPanel(new FlowLayout());
 		p_drawCanvas = new JPanel();
 		p_btnbar = new JPanel();
 		btn_clear = new JButton("지우기");
 		btn_clear.addActionListener(this);
+		makeCanvas = new MakeCanvas();
 		
-		newCanvas = new NewCanvas();
-		newCanvas.setVisible(true);
-		newCanvas.setBackground(Color.WHITE);
+		makeCanvas.setVisible(true);
+		makeCanvas.setBackground(Color.WHITE);
 		
+		p_layout.add(p_drawCanvas);
+		p_layout.add(p_btnbar);
 		
-		p_drawCanvas.add(newCanvas);
+		p_drawCanvas.add(makeCanvas);
 		p_btnbar.add(btn_clear);
 		
-		add(p_drawCanvas);
-		add(p_btnbar);
+
+
+		//라벨에 layout종류 넣고 캔버스 만들기 (메인에서 되면 붙이기)
 		
+	}
+	public void display() {
 		setSize(500, 500);
 		setVisible(true);
-		setLayout(new GridLayout(2, 1));
-		//라벨에 layout종류 넣고 캔버스 만들기 (메인에서 되면 붙이기)
+		setLayout(new FlowLayout());
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		add(p_layout);
 		
 	}
 
@@ -66,8 +75,8 @@ public class Draw extends JFrame implements ActionListener, MouseMotionListener{
 	}
 }
 
-class NewCanvas extends Canvas {
-	int x = 750, y = 750;
+class MakeCanvas extends Canvas {
+	int x = 100, y = 100;
 	
 	public void paint(Graphics g) {
 		g.setColor(Color.BLACK);
